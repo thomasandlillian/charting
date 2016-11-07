3a298703ef8b5ead5734a54ce7b55c6afe940c8c
Thomas Hansen <thomashansen@Thomass-MacBook-Air.local>
2016-11-07 14:13:39 -0700

    Initial Commit

---
+++ charting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  charting
+//
+//  Created by Thomas Hansen on 11/7/16.
+//  Copyright © 2016 Thomas Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
