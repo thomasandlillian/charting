f1d487e2e23aa858d85ac9e5e337b912cf42c5f4
Thomas Hansen <thomashansen@Thomass-MacBook-Air.local>
2016-11-07 14:16:05 -0700

    Hello World

--- charting/ViewController.swift
+++ charting/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+        print ("Hello World")
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
